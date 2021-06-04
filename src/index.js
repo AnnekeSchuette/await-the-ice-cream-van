7153964d603d9c42bec51be29d47adffeb526c99
Anneke Schütte <a.schuette.hh@gmail.com>
2021-06-04 22:20:12 +0200

    Create data basis and general structure

---
+++ src/index.js
@@ -0,0 +1,45 @@
+let stocks = {
+  fruits: [
+    {name: "Strawberry", icon: "🍓"},
+    {name: "Pineapple", icon: "🍍"},
+    {name: "Banana", icon: "🍌"},
+    {name: "Cherry", icon: "🍒"},
+    {name: "Lemon", icon: "🍋"},
+    {name: "Peach", icon: "🍑"},
+    {name: "Pear", icon: "🍐"},
+    {name: "Orange", icon: "🍊"},
+    {name: "Grapes", icon: "🍇"},
+    {name: "Blueberries", icon: "🫐"},
+    {name: "Mango", icon: "🥭"},
+    {name: "Apple", icon: "🍎"},
+    {name: "Water Melon", icon: "🍉"},
+    {name: "Honey Melon", icon: "🍈"}
+  ],
+  liquid : ["water", "milk", "ice"],
+  holder: ["Cone", "Cup", "Stick"],
+  toppings: ["Chocolate", "Sprinkles"]
+}
+
+let order = (callProduction) => {
+  console.log("Order placed.")
+  console.log("Initiate production.")
+  callProduction()
+}
+
+let production = () => {
+  console.log("Production started")
+}
+
+stocks.fruits.forEach(({name, icon}) => addMenuItem(`${icon} ${name}`, '.fruits'))
+stocks.holder.map(holder => addMenuItem(holder, '.holder'))
+stocks.toppings.map(topping => addMenuItem(topping, '.toppings'))
+
+function addMenuItem(el, target){
+  let menu = document.querySelector(target)
+  let li = document.createElement('li')
+  let text = document.createTextNode(el)
+  li.appendChild(text)
+  return menu.appendChild(li)
+}
+
+order(production)
