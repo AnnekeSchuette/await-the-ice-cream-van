9f7b23c5549de4677db9a044a3f33082e90515d6
Anneke Schütte <a.schuette.hh@gmail.com>
2021-06-06 22:36:33 +0200

    Extend process, create loop process function, add print dialogue function, refactoring

--- src/index.js
+++ src/index.js
@@ -1,49 +1,64 @@
+let is_shop_open = true
+
 let stocks = {
   fruits: [
-    {name: "Strawberry", icon: "🍓"},
-    {name: "Pineapple", icon: "🍍"},
-    {name: "Banana", icon: "🍌"},
-    {name: "Cherry", icon: "🍒"},
-    {name: "Lemon", icon: "🍋"},
-    {name: "Peach", icon: "🍑"},
-    {name: "Pear", icon: "🍐"},
-    {name: "Orange", icon: "🍊"},
-    {name: "Grapes", icon: "🍇"},
-    {name: "Blueberries", icon: "🫐"},
-    {name: "Mango", icon: "🥭"},
-    {name: "Apple", icon: "🍎"},
-    {name: "Water Melon", icon: "🍉"},
-    {name: "Honey Melon", icon: "🍈"}
+    {name: "strawberry", icon: "🍓"},
+    {name: "pineapple", icon: "🍍"},
+    {name: "banana", icon: "🍌"},
+    {name: "cherry", icon: "🍒"},
+    {name: "lemon", icon: "🍋"},
+    {name: "peach", icon: "🍑"},
+    {name: "pear", icon: "🍐"},
+    {name: "orange", icon: "🍊"},
+    {name: "grapes", icon: "🍇"},
+    {name: "blueberries", icon: "🫐"},
+    {name: "mango", icon: "🥭"},
+    {name: "apple", icon: "🍎"},
+    {name: "water melon", icon: "🍉"},
+    {name: "honey melon", icon: "🍈"}
   ],
-  liquid : ["Water", "Milk", "Ice"],
-  holder: ["Cone", "Cup", "Stick"],
-  toppings: ["Chocolate", "Sprinkles"]
+  liquid : ["water", "milk", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "sprinkles"]
 }
+let prompt = () => prompt("Please enter your name", "Harry Potter")
 
 let steps = [
-  {name: "place order", time: 3000},
-  {name: "fetch ingredients", time: 3000},
-  {name: "cut the fruit", time: 2000},
-  {name: "add fluids and ice", time: 1000},
-  {name: "start the machine", time: 1000},
-  {name: "select container", time: 2000},
-  {name: "select topping", time: 3000},
-  {name: "serve ice cream", time: 2000}
+  {name: "greeting", time: 1000, text: `Hello, dear friend!`},
+  {name: "info", time: 1000, text: `We've-a got-a many sorts of flavors, you know. \n Just take a look-a at the menu.`},
+  {name: "question", time: 3000, text: `Which-a ice cream flavor do you desire?`},
+  {name: "place order", time: 3000, text: `${stocks.fruits[1].name} selected`},
+  {name: "fetch ingredients", time: 3000, text: `${stocks.fruits[1].name} fetched from stocks`},
+  {name: "cut the fruit", time: 2000, text: `fruit cut`},
+  {name: "add liquid and ice", time: 1000, text: `${stocks.liquid[1]} added`},
+  {name: "start the machine", time: 1000, text: `machine was started, timer is running (5s)`},
+  {name: "machine beeps", time: 5000, text: `Ice cream is ready`},
+  {name: "select container", time: 2000, text: `ice cream placed on ${stocks.holder[0]}`},
+  {name: "select topping", time: 3000, text: `${stocks.toppings[0]} added as toppings`},
+  {name: "serve ice cream", time: 2000, text: `ice cream served`}
 ]
 
-let order = (callProduction) => {
-  console.log("Order placed.")
-  console.log("Initiate production.")
-  callProduction()
+let order = (time, work) => {
+  return new Promise( ( resolve, reject ) =>{
+    if( is_shop_open ){
+      setTimeout(()=>{
+
+        // work is 👇 getting done here
+         resolve( work() )
+
+        // Setting 👇 time here for 1 work
+        }, time)
+    }
+    else {
+      reject( console.log("Our shop is closed") )
+    }
+  })
 }
 
-let production = () => {
-  console.log("Production started")
-}
-
-stocks.fruits.forEach(({name, icon}) => addMenuItem(`${icon} ${name}`, '.menu__fruits'))
-stocks.holder.map(holder => addMenuItem(holder, '.menu__holder'))
-stocks.toppings.map(topping => addMenuItem(topping, '.menu__toppings'))
+// create menu card from stocks/ingredients
+let fruits = stocks.fruits.forEach(({name, icon}) => addMenuItem(`${icon} ${name}`, '.menu__fruits'))
+let holder = stocks.holder.map(holder => addMenuItem(holder, '.menu__holder'))
+let toppings = stocks.toppings.map(topping => addMenuItem(topping, '.menu__toppings'))
 
 function addMenuItem(el, target){
   let menu = document.querySelector(target)
@@ -53,4 +68,15 @@ function addMenuItem(el, target){
   return menu.appendChild(li)
 }
 
-order(production)
+function printDialog(text) {
+  const dialogue = document.querySelector('.dialogue')
+  dialogue.innerText = text
+}
+
+async function* processProduction(steps){
+  for (let i = 0; i < steps.length; i++) yield order(steps[i].time, () => console.log(`${i+1}: ${steps[i].name}`)).then(() => i)
+}
+
+(async function loopSteps() {
+  for await (let i of processProduction(steps)) printDialog(steps[i].text);
+})();
